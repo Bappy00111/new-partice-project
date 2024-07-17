ea25904f55f9c283c6c356c8a4d6365799825181
Bappy Modok <bappymodok0011@gmail.com>
2024-07-17 14:39:33 +0600

    banner section modity

--- src/components/Banner.jsx
+++ src/components/Banner.jsx
@@ -1,15 +1,7 @@
 import React, { useState } from "react";
-// import React, { useRef, useState } from 'react';
-// Import Swiper React components
 import { Swiper, SwiperSlide } from "swiper/react";
-
-// Import Swiper styles
 import "swiper/css";
 import "swiper/css/pagination";
-
-// import './styles.css';
-
-// import required modules
 import { Autoplay, Pagination } from "swiper/modules";
 
 const images = [
@@ -20,44 +12,45 @@ const images = [
 ];
 
 const Banner = () => {
+  const [transform, setTransform] = useState({ x: 0, y: 0 });
+
+  const handleMouseMove = (e) => {
+    const { clientX, clientY, currentTarget } = e;
+    const { offsetWidth, offsetHeight } = currentTarget;
+    const moveX = ((clientX / offsetWidth) - 0.5) * 30; // 30px পর্যন্ত ডান-বাম সরে
+    const moveY = ((clientY / offsetHeight) - 0.5) * 30; // 30px পর্যন্ত উপরে-নিচে সরে
+    setTransform({ x: moveX, y: moveY });
+  };
+
+  const handleMouseLeave = () => {
+    setTransform({ x: 0, y: 0 });
+  };
+
   return (
     <div>
-      <Swiper 
-      pagination={true}
-       modules={[Pagination,Autoplay]} 
-       className="mySwiper"
-        autoplay={{ delay: 3000 }} 
-        >
+      <Swiper
+        pagination={true}
+        modules={[Pagination, Autoplay]}
+        className="mySwiper"
+        autoplay={{ delay: 3000 }}
+      >
         {images?.map((img, index) => (
-          <SwiperSlide key={index} className="h-[calc(100vh-200px)] ">
+          <SwiperSlide
+            key={index}
+            className="h-[calc(100vh-200px)]"
+            onMouseMove={handleMouseMove}
+            onMouseLeave={handleMouseLeave}
+          >
             <img
-              className="w-full h-full bg-cover bg-center"
+              className="w-full h-full bg-cover bg-center transition-transform duration-200 ease-out"
               src={img}
               alt=""
+              style={{
+                transform: `translate(${transform.x}px, ${transform.y}px)`,
+              }}
             />
           </SwiperSlide>
         ))}
-        {/* <SwiperSlide>
-          <img
-            className="w-full"
-            src="https://animares.com/clickandbuilds/ANIMARES3DmedicalanimationIARIMRIVR/wp-content/uploads/2022/08/IK_OR_2048x1000_tiny.jpg"
-            alt=""
-          />
-        </SwiperSlide>
-        <SwiperSlide>
-          <img
-            className="w-full"
-            src="https://animares.com/clickandbuilds/ANIMARES3DmedicalanimationIARIMRIVR/wp-content/uploads/2020/09/InsightLung_2048x1000_tiny.jpg"
-            alt=""
-          />
-        </SwiperSlide>
-        <SwiperSlide>
-          <img
-            className="w-full"
-            src="https://animares.com/clickandbuilds/ANIMARES3DmedicalanimationIARIMRIVR/wp-content/uploads/2019/06/uni_vl_AR_26010619_11_1024x500_Logos_v003.jpg"
-            alt=""
-          />
-        </SwiperSlide> */}
       </Swiper>
     </div>
   );
